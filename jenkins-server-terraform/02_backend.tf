e93e1f700d09d9c444b23a78c1f53bcca591e0a4
melvinizunna <98717216+melvinizunna@users.noreply.github.com>
2025-02-11 20:02:53 -0500

    Update terrafoam 02_backend.tf
    Updated the terrafoam credentials and path for storing stateful data

--- jenkins-server-terraform/02_backend.tf
+++ jenkins-server-terraform/02_backend.tf
@@ -2,16 +2,16 @@
 terraform {
   backend "s3" {
     # The S3 bucket where the Terraform state file will be stored.
-    bucket         = "three-tier-devsecops-project-bucket-s3"
+    bucket         = "Kuberbucket"
     
     # The AWS region where the S3 bucket and DynamoDB table are located.
-    region         = "us-west-2"
+    region         = "us-east-1"
     
     # The key (path) within the S3 bucket for storing the state file.
-    key            = "three-tier-devdecops-project/jenkins-server-terraform/terraform.tfstate"
+    key            = "kuberbucket/jenkins-server-terraform/terraform.tfstate"
     
     # The DynamoDB table used for state locking to prevent concurrent state changes.
-    dynamodb_table = "lock-files"
+    dynamodb_table = "kubertable"
     
     # Ensures the state file is encrypted at rest in the S3 bucket.
     encrypt        = true
